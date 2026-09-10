c8ba08fe20bfe5f17637c5da11393dcdf0dad0db
Stefanie Nogueira <stefanie.mattos72@gmail.com>
2026-09-09 21:27:50 -0300

    feat: inicio do projeto JSON - PLACEHOLDER

---
+++ script.js
@@ -0,0 +1,46 @@
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const listaPosts = document.getElementById('listaPosts');
+const loading = document.getElementById('loading');
+const mensagem = document.getElementById('mensagem');.
+const btnCarregarPosts = document.getElementById('carregar-posts');
+
+btnCarregarPosts.addEventListener('click', carregarPosts);
+
+async function carregarPosts() {  
+    loading.style.display = 'block';  
+    listaPosts.innerHTML = '';
+    mensagem.textContent = '';
+    // Show loading indicator
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error('Erro ao carregar os posts.');
+        }
+        const posts = await response.json();
+        //mostrarPosts(posts);
+        console.log(posts);
+
+    } catch (error) {
+        mensagem.textContent = 'Erro ao carregar os posts.';
+    } finally {
+        // Hide loading indicator
+        loading.style.display = 'none';
+    }
+}
+function mostrarPosts(posts) {
+    posts.forEach(post => {
+        const div = document.createElement('div');
+        div.classList.add('post');
+        div.innerHTML = `
+            <div class="post-id">
+            Post # ${post.id}
+            </div>
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+        `;
+        listaPosts.appendChild(div);
+    });
+}
+//      listaPosts.innerHTML = '';
+//      mensagem.textContent = '';
