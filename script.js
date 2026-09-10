b4d54d59fc082c47fd2880fc3e2f9d9a6dc1737b
Stefanie Nogueira <stefanie.mattos72@gmail.com>
2026-09-09 21:53:22 -0300

    feat: layout grid dos posts

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_URL = 'https://jsonplaceholder.typicode.com/posts';
 
 const listaPosts = document.getElementById('listaPosts');
 const loading = document.getElementById('loading');
-const mensagem = document.getElementById('mensagem');.
+const mensagem = document.getElementById('mensagem');
 const btnCarregarPosts = document.getElementById('carregar-posts');
 
 btnCarregarPosts.addEventListener('click', carregarPosts);
@@ -11,20 +11,19 @@ async function carregarPosts() {
     loading.style.display = 'block';  
     listaPosts.innerHTML = '';
     mensagem.textContent = '';
-    // Show loading indicator
+
     try {
         const response = await fetch(API_URL);
         if (!response.ok) {
             throw new Error('Erro ao carregar os posts.');
         }
         const posts = await response.json();
-        //mostrarPosts(posts);
+        mostrarPosts(posts);
         console.log(posts);
 
     } catch (error) {
         mensagem.textContent = 'Erro ao carregar os posts.';
     } finally {
-        // Hide loading indicator
         loading.style.display = 'none';
     }
 }
